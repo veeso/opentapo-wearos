bd26ab1f53fd780f0f2be64bf05c90ae01eb7751
veeso <christian.visintin1997@gmail.com>
2023-01-27 15:24:58 +0100

    Groups

--- OpenTapoWearOs/app/src/main/java/dev/veeso/opentapowearos/view/app_data/DeviceGroups.kt
+++ OpenTapoWearOs/app/src/main/java/dev/veeso/opentapowearos/view/app_data/DeviceGroups.kt
@@ -1,18 +1,51 @@
 package dev.veeso.opentapowearos.view.app_data
 
+import android.util.Log
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 class DeviceGroups {
 
-    companion object {
-        fun serialize(groups: Map<String, List<String>>): String {
-            return Json.encodeToString(groups)
-        }
+    private var groups: MutableMap<String, List<String>>
 
-        fun deserialize(payload: String): Map<String, List<String>> {
-            return Json.decodeFromString(payload)
+    init {
+        this.groups = mutableMapOf()
+    }
+
+    constructor(payload: String) {
+        this.groups = Json.decodeFromString(payload)
+    }
+
+    fun add(name: String, devices: List<String>) {
+        if (groups.contains(name)) {
+            throw Exception(String.format("Group '%s' already exists", name))
         }
+        Log.d(TAG, String.format("Adding group %s with devices %s", name, devices))
+        groups[name] = devices
+    }
+
+    fun remove(name: String) {
+        Log.d(TAG, String.format("Removing group %s", name))
+        groups.remove(name)
+    }
+
+    fun serialize(): String {
+        return Json.encodeToString(groups)
+    }
+
+    fun getNames(): List<String> {
+        return groups.keys.toList()
+    }
+
+    fun getDevices(name: String): List<String> {
+        if (!groups.contains(name)) {
+            throw Exception(String.format("Group '%s' doesn't exist", name))
+        }
+        return groups[name]!!
+    }
+
+    companion object {
+        const val TAG = "DeviceGroups"
     }
 }
