88b38626fb326aad53c575a2a440c109b1f64090
Christian Visintin <christian.visintin1997@gmail.com>
2023-01-30 15:57:17 +0100

    Scan devices ONLY when there's no cached device (#10)
    * simplified onCredentials logic
    
    * Don't scan for cached devices, sign in on the fly

--- OpenTapoWearOs/app/src/main/java/dev/veeso/opentapowearos/view/app_data/DeviceCache.kt
+++ OpenTapoWearOs/app/src/main/java/dev/veeso/opentapowearos/view/app_data/DeviceCache.kt
@@ -1,27 +1,63 @@
 package dev.veeso.opentapowearos.view.app_data
 
+import dev.veeso.opentapowearos.tapo.device.Device
+import dev.veeso.opentapowearos.tapo.device.DeviceBuilder
+import dev.veeso.opentapowearos.tapo.device.DeviceModel
+import dev.veeso.opentapowearos.tapo.device.DeviceStatus
+import dev.veeso.opentapowearos.view.intent_data.DeviceData
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.decodeFromString
 
-class DeviceCache(devices: List<String>) {
+class DeviceCache(devices: List<Device>) {
 
-    private var devices: MutableList<String>
+    private var devices: List<Device>
 
     init {
         this.devices = devices.toMutableList()
     }
 
     constructor(payload: String) : this(listOf()) {
-        this.devices = Json.decodeFromString(payload)
+        val deviceData: List<CachedDevice> = Json.decodeFromString(payload)
+        this.devices = deviceData.map {
+            DeviceBuilder.buildDevice(
+                it.alias,
+                it.id,
+                it.model,
+                it.endpoint,
+                it.ipAddress,
+                it.status
+            )
+        }
+
     }
 
     fun serialize(): String {
-        return Json.encodeToString(devices)
+        val serialized: List<CachedDevice> = this.devices.map {
+            CachedDevice(
+                it.alias,
+                it.id,
+                it.model,
+                it.endpoint,
+                it.ipAddress,
+                it.status
+            )
+        }
+        return Json.encodeToString(serialized)
     }
 
-    fun devices(): List<String> {
-        return this.devices.toList()
+    fun devices(): List<Device> {
+        return this.devices
     }
 
 }
+
+@kotlinx.serialization.Serializable
+data class CachedDevice(
+    val alias: String,
+    val id: String,
+    val model: DeviceModel,
+    val endpoint: String,
+    val ipAddress: String,
+    val status: DeviceStatus
+)
